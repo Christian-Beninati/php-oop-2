616c26f488b285ce56ab9ad9d7b05bbf13954ed9
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-24 17:10:20 +0100

    Class Shop

---
+++ models/shop.php
@@ -0,0 +1,23 @@
+<?php
+// Class Product
+require_once __DIR__ . '/product.php';
+// Class Category
+require_once __DIR__ . '/category.php';
+
+// Shop class definition
+class Shop
+{
+    public $categories;
+
+    // Construct
+    public function __construct()
+    { // Initialise the empty category array at the beginning
+        $this->categories = array();
+    }
+
+    // Method for adding a category to the shop
+    public function addCategory($category)
+    { // Adds the category to the shop's category array
+        $this->categories[] = $category;
+    }
+}
