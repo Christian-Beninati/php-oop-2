f66003c5c21d6fdf40b410f838bd1bae35304455
Christian-Beninati <christianbeninati.info@gmail.com>
2023-07-24 22:47:14 +0100

    product_data

---
+++ data/product_data.php
@@ -0,0 +1,26 @@
+<?php
+
+// DOG
+$dogProducts = [
+    new Product(1, 'Acana', 'Acana food is high in protein and nutrients, and it is perfect for dogs of all ages and sizes.', 10.99, $dogCategory, 'https://shorturl.at/adiu4'),
+    new Product(5, 'Ruffwear', 'Made of high quality nylon and features snap buckle that is easy to open and close.', 5.99, $dogCategory, 'https://shorturl.at/dvx03'),
+    new Product(6, 'Ruffwear', 'Made of durable and safe stainless steel for your dog.', 12.99, $dogCategory, 'https://shorturl.at/jm045')
+];
+
+// CAT
+$catProducts = [
+    new Product(2, 'Orijen', 'Orijen is a premium cat food brand that is made with fresh, local ingredients.', 8.99, $catCategory, 'https://shorturl.at/elmzH'),
+    new Product(9, 'Ferplast', 'They are available in a variety of shapes and sizes, so you can find the perfect one for your cat.', 35.40, $catCategory, 'https://shorturl.at/jxHT9')
+];
+
+// BIRD
+$birdProducts = [
+    new Product(3, 'Prevue Hendryx', 'Prevue Hendryx aviaries are made of durable, high-quality materials', 105.49, $birdCategory, 'https://shorturl.at/qxLQ2')
+];
+
+// FISH
+$fishProducts = [
+    new Product(4, 'Fluval', 'The Fluval filter is made with premium materials to keep your aquarium water clean and healthy.', 42.49, $fishCategory, 'https://shorturl.at/iADQY'),
+    new Product(7, 'TetraMin', 'Complete and balanced food for freshwater fish. TetraMin is available in a variety of formats', 9.99, $fishCategory, 'https://shorturl.at/hLVX4'),
+    new Product(8, 'Juwel', 'They are available in a variety of sizes and shapes, designed to be easy to install and maintain.', 85.49, $fishCategory, 'https://shorturl.at/osAKO')
+];
